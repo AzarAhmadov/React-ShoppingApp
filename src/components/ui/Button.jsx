9f228fdb7f559234afacc30dd3add0d4490ec02b
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-14 21:52:06 +0400

    Update Button.jsx

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,7 @@
-import React, { memo, mmeo } from "react";
+import React, { memo } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Button = ({ type, children, to }) => {
+const Button = ({ type, children, to, disabled }) => {
   const navigate = useNavigate();
 
   const handleNavigate = () => {
@@ -12,9 +12,10 @@ const Button = ({ type, children, to }) => {
 
   return (
     <button
+      disabled={disabled}
       onClick={handleNavigate}
       type={type}
-      className="w-full px-4 py-2 text-white bg-black rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300"
+      className="w-full px-4 py-2 text-white bg-black rounded-lg disabled:opacity-40 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300"
     >
       {children}
     </button>
